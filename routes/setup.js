afd37b4b5f23e73d79e8bcc44d21932fbf8cafa8
EthanVB123 <ethanvanbruchem@gmail.com>
2021-03-30 20:25:46 +0800

    Fixed setup and creating player - added /setup/teapot

--- routes/setup.js
+++ routes/setup.js
@@ -1,19 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const game = require('../models/game')
-const player = require('../models/player')
+const Player = require('../models/player')
 
-router.get('/', (req, res) => {
-    res.redirect('/')
-})
-router.get('/:id', (req, res) => {
-    res.send(`${req.params.id}'s Game`)
+// Index route redirects to homepage
+router.get('/', async (req, res) => {
+    //try {
+        allPlayerNames = ''
+        const players = await Player.find({})
+        players.forEach(element => {
+            allPlayerNames += element.name
+            allPlayerNames += ' '
+        });
+        res.send(allPlayerNames)
+    //} catch {
+        //res.redirect('/')
+        //console.error('Error with index route')        
+    //} 
 })
 
 // New Player Route
 router.get('/new', (req, res) => {
-    res.render('players/new', {author: new Author()})
+    res.render('players/new', {player: new Player()})
 })
 
+router.post('/', async (req, res) => {
+    const player = new Player({
+        name: req.body.name
+    })
+    try {
+        const newPlayer = await player.save()
+        res.redirect(`setup/${newPlayer.name}`)
+    } catch {
+        res.redirect('/')
+        console.error('Error creating Player')
+    }
+})
+
+// I'm a teapot
+router.get('/teapot', (req, res) => {
+    res.sendStatus(418)
+})
+
+// Joined Game Route
+router.get('/:id', (req, res) => {
+    res.send(`${req.params.id}'s Game`)
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
-//const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 //const methodOverride = require('method-override')
 
 // Here are my routers
@@ -18,7 +18,7 @@ app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 //app.use(methodOverride("_method"))
 app.use(express.static('public'))
-//app.use(bodyParser.urlencoded({limit: "10mb", extended: false}))
+app.use(bodyParser.urlencoded({limit: "10mb", extended: false}))
 
 // Mongoose stuff goes here
 const mongoose = require('mongoose')
