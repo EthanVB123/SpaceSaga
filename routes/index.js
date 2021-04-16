db5e90caeb25d129f2fcdf5d9110d54b6e25ae44
EthanVB123 <ethanvanbruchem@gmail.com>
2021-04-16 11:31:07 +0800

    Added short credits message

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Game = require('../models/game')
-
+const alert = require('alert')
 router.get('/', async (req, res) => {
     try {
         const games = await Game.find({})
@@ -10,6 +10,10 @@ router.get('/', async (req, res) => {
         console.error('Error finding games')
     }
     
+})
+router.get('/credits', (req, res) => {
+    alert('Created by Ethan van Bruchem, in 2021 \n Thanks for playing!')
+    res.redirect('/')
 })
 
 module.exports = router
